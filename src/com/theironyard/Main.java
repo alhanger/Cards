96af86d052019f5064676857e5be5c9182b807c2
alhanger <hanger129@gmail.com>
2015-11-30 16:49:20 -0500

    Make isStraight and isStraightFlush methods

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,5 +1,6 @@
 package com.theironyard;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.stream.Collectors;
 
@@ -50,10 +51,62 @@ public class Main {
         return suits.size() == 1;
     }
 
+    static boolean isStraight(ArrayList<Card> hand) {
+        ArrayList<Integer> ranks = hand.stream()
+                                        .map(card -> {
+                                            return card.rank.ordinal();
+                                        })
+                                        .sorted()
+                                        .collect(Collectors.toCollection(ArrayList::new));
+
+        ArrayList<Integer> rankCheck = new ArrayList<>();
+        int i = ranks.get(0);
+        for (int r = 0; r < ranks.size(); r++) {
+            rankCheck.add(i + r);
+        }
+
+        return ranks.equals(rankCheck);
+    }
+
+    static boolean isStraightFlush(ArrayList<Card> hand) {
+        HashSet<Card> newHand = new HashSet<>(hand);
+
+        return isFlush(newHand) && isStraight(hand);
+    }
+
+    static boolean isFour(HashSet<Card> hand) {
+        HashSet<Card.Rank> ranks = hand.stream()
+                                        .map(card -> {
+                                            return card.rank;
+                                        })
+                                        .collect(Collectors.toCollection(HashSet::new));
+
+        return ranks.size() == 1;
+    }
+
+//    static boolean isThree(HashSet<Card> hand) {
+//        HashSet<Card.Rank> ranks = hand.stream()
+//                                        .map(card -> {
+//                                            return card.rank;
+//                                        })
+//                                        .collect(Collectors.toCollection(HashSet::new));
+//
+//
+//    }
+
     public static void main(String[] args) {
         long beginTime = System.currentTimeMillis();
 
         Card aceOfSpades = new Card(Card.Suit.SPADE, Card.Rank.ACE);
+        Card twoOfSpades = new Card(Card.Suit.SPADE, Card.Rank.TWO);
+        Card threeOfSpades = new Card(Card.Suit.SPADE, Card.Rank.THREE);
+        Card fourOfSpades = new Card(Card.Suit.SPADE, Card.Rank.SIX);
+        ArrayList<Card> hand = new ArrayList<>();
+        hand.add(aceOfSpades);
+        hand.add(twoOfSpades);
+        hand.add(threeOfSpades);
+        hand.add(fourOfSpades);
+        System.out.println(isStraight(hand));
         HashSet<Card> deck = createDeck();
         //System.out.println(deck.size());
         //System.out.println(deck.contains(aceOfSpades));
